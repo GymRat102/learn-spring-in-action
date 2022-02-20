477001f2fefb704c4eabd540b8f9e13acd979527
GymRat102 <weijianduan0302@gmail.com>
2022-02-20 17:49:30 +0800

    [chapter1] init project

---
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class) // not come with initializer project, and need to add Junit4 to classpath
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
